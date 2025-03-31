638bd2036c47f1c0166188eb31170ff1a8f6e9d0
Untroubled-cyclist <elliot.agnew@verizon.net>
2025-03-31 00:08:34 -0500

    Move API handler out from server.js

--- server/server.js
+++ server/server.js
@@ -3,15 +3,9 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 
-const app = express();
+const api = require('./api.js');
 
-app.use(express.static(path.join(__dirname, '../dist')));
-// app.use('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../index.html'));
-// });
-// app.use('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../dist', 'index.html'));
-// });
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -26,47 +20,8 @@ app.use(session({
     }
 }));
 
-// block unauthenticated requests
-app.use((req, res, next) => {
-    if (req.path === '/api/login' || (req.session.valid && req.session.expiresAt >= Date.now()))
-        return next();
-
-    return res.status(401).send('Unauthenticated');
-});
-
-app.get('/', (req, res) => {
-    res.send('Home page, should probably host front-end content here');
-});
-
-app.post('/api/login', (req, res) => {
-    console.log('login');
-    const email = req.body.email;
-    if (email === 'admin@email.com') {
-        req.session.valid = true;
-        req.session.expiresAt = Date.now() + req.session.cookie.maxAge;
-        console.log('User authenticated');
-        return res.json({
-            username: 'Admin',
-            isAdmin: false,
-            validUntil: req.session.expiresAt,
-        });
-    }
-
-    console.log('user not authenticated');
-    return res.status(400).send('Invalid username or password');
-});
-
-app.post('/api/logout', (req, res) => {
-    if (req.session.valid)
-        return res.status(400).send('Not logged in');
-
-    req.session.destroy();
-    return res.send();
-});
-
-app.get('/test', (req, res) => {
-    res.send('Ping!');
-});
+app.use('/api', api);
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.listen(5173, () => {
     console.log('Server is running');
