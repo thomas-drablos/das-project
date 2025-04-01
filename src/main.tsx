4c89eef6ba1374fc74fdafe34869311ab0e93790
Matthew Sheldon <punchwood2003@gmail.com>
2025-04-01 00:52:38 -0500

    feat: added support for Auth0 login

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
 import 'bulma/css/bulma.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import App from './App.tsx'
@@ -11,7 +12,16 @@ if (root == null) {
 } else {
   createRoot(root).render(
     <StrictMode>
-      <App />
+      {/* Required for Auth0 Authentication/Login */}
+      <Auth0Provider
+        domain="dev-olcmjrm1xuqtgb8o.us.auth0.com"
+        clientId="zqvb0HiU9R5WVx2KObs9wGepXr46sTwO"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <App />
+      </Auth0Provider>
     </StrictMode>,
   )
 }
