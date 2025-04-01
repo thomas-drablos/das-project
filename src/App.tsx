73a8362c99597583b1abf0ad24581885c8c48093
Matthew Sheldon <punchwood2003@gmail.com>
2025-04-01 00:53:22 -0500

    feat: created dummy login application to show Auth0 working

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,44 @@
 import React, { useState } from 'react'
+import { useAuth0 } from '@auth0/auth0-react';
 
-import LoginForm from './LoginForm.tsx'
+import { ButtonField } from './Bulma.tsx'
 import Navbar from './Navbar.tsx'
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } = useAuth0()
   const [activePage, setActivePage] = useState('Page1')
 
   return (
     <>
       <Navbar
         activePage={activePage}
-        isLoggedIn={isLoggedIn}
-        onLogOut={() => { setIsLoggedIn(false) }}
+        isLoggedIn={isAuthenticated}
+        onLogOut={() => logout({ returnTo: window.location.origin })}
         pages={['Page1', 'Page2', 'Page3']}
         setActivePage={setActivePage}
-        />
+      />
       <section className="section">
-        {!isLoggedIn && (
-          <div className="columns is-centered">
-            <div className="column" style={{ maxWidth: '600px' }}>
-              <LoginForm onSuccess={() => { setIsLoggedIn(true) }} />
-            </div>
+        {!isAuthenticated && (
+          <div className="content">
+            <h1>Welcome to Comet Commerce!</h1>
+            <p>You are not logged in yet.</p>
+            <ButtonField kind="primary" loading={isLoading} onClick={loginWithRedirect}>
+              Log in
+            </ButtonField>
           </div>
         )}
+        {isAuthenticated && (
+          <div className="content">
+            <h1>Welcome to Comet Commerce, {user.name}!</h1>
+            <p>You are logged in.</p>
+          </div>
+        )}
+        {isLoading && (
+          <div className="content">
+            <p>Loading...</p>
+          </div>
+        )}
+        {error && alert(`Error: ${error.message}`)}
       </section>
     </>
   )
