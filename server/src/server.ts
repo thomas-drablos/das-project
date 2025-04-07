0ff03e2531c383f451190e6408c8821319d7f42e
Isabella Pereira <isabellapereira251@gmail.com>
2025-04-07 13:28:10 -0500

    Correction to mongoose connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -31,14 +31,10 @@ app.use(session({
 }));
 
 app.use('/api', api);
-// app.use(express.static(path.join(__dirname, '../dist')));
 
-
+require("dotenv").config()
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb+srv://cometcommerce6348:i1OlL9T3HsAp6jXc@cluster0.coh9pgj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-  //useNewUrlParser: true,
-  //useUnifiedTopology: true,
-})
+mongoose.connect(process.env.DB_CONN_STRING || '');
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
