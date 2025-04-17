980e5d7b81a31f26d16d52d6270ff6b168ae427c
Isabella Pereira <118389802+ipereira251@users.noreply.github.com>
2025-04-17 14:18:29 -0500

    Revert "Db endpoints"

--- server/src/models/invoice.ts
+++ server/src/models/invoice.ts
@@ -1,41 +1,13 @@
-import mongoose, { Schema, Document, model } from 'mongoose';
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+const invoiceSchema = new Schema({
+    user: Object, 
+    vendor: Object, 
+    time: Date, 
+    price: Number, 
+    paid: Boolean, 
+    specs: String
 
-export interface IInvoice extends Document {
-  user: {
-    id: string;
-    name: string;
-    email?: string;
-  };
-  vendor: {
-    id: string;
-    name: string;
-    email?: string;
-  };
-  time: Date;
-  price: number;
-  paid: boolean;
-  specs: string;
-}
-
-export const invoiceSchema = new Schema<IInvoice>(
-  {
-    user: {
-      id: { type: String, required: true },
-      name: { type: String, required: true },
-      email: { type: String },
-    },
-    vendor: {
-      id: { type: String, required: true },
-      name: { type: String, required: true },
-      email: { type: String },
-    },
-    time: { type: Date, default: Date.now },
-    price: { type: Number, required: true },
-    paid: { type: Boolean, default: false },
-    specs: { type: String, required: true },
-  },
-  { timestamps: true }
-);
-
-const Invoice = model<IInvoice>('Invoice', invoiceSchema);
+});
+const Invoice = model('Invoice', invoiceSchema);
 export default Invoice;
--- server/src/models/review.ts
+++ server/src/models/review.ts
@@ -1,31 +1,12 @@
-import mongoose, { Schema, Document, model } from 'mongoose';
-import User, { IUser } from './user';
-import { IVendor } from './vendor';
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+const reviewSchema = new Schema({
+   user: Object, 
+   vendor: Object, 
+   text: String, 
+   rating: Number, 
+   time: Date
 
-export interface IReview extends Document {
-  user: IUser;
-  vendor: IVendor;
-  text: string;
-  rating: number;
-  time: Date;
-}
-
-export const reviewSchema = new Schema<IReview>(
-  {
-    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    vendor: { type: Schema.Types.ObjectId, ref: 'Vendor', required: true},
-    text: { type: String, required: true },
-    rating: {
-      type: Number,
-      required: true,
-      min: 1,
-      max: 5,
-    },
-    time: { type: Date, default: Date.now },
-  },
-  { timestamps: true }
-);
-
-const Review = model<IReview>('Review', reviewSchema);
-module.exports = Review;
+});
+const Review = model('Review', reviewSchema);
 export default Review;
