b48bb552f139e24e62fee26211a82b5cc793adc9
Untroubled-cyclist <elliot.agnew@verizon.net>
2025-04-21 21:22:30 -0500

    Documented enabling TLS encryption

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,23 @@ import fs from 'fs';
 const SSL_PRIVATE_KEY = 'secrets/web.key';
 const SSL_CERTIFICATE = 'secrets/web.crt';
 const API_CERTIFICATE = 'secrets/api.crt';
-const ENABLE_HTTPS = true;
+const ENABLE_HTTPS = false;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: ENABLE_HTTPS && {
+    https: ENABLE_HTTPS ? {
       key: fs.readFileSync(SSL_PRIVATE_KEY),
       cert: fs.readFileSync(SSL_CERTIFICATE),
-    },
+    } : undefined,
     proxy: {
       '/api': {
         target: {
           protocol: 'https:',
           host: 'localhost',
           port: 8000,
-          ca: fs.readFileSync(API_CERTIFICATE).toString(),
+          ca: ENABLE_HTTPS ? fs.readFileSync(API_CERTIFICATE).toString() : undefined,
         },
         changeOrigin: true,
         secure: true,
